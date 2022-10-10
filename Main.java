f44bc576f8fdc5ac4ab7f475e2bbfd0f3b89c7cf
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2022-10-10 17:29:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ import java.util.Objects;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        var console = new Console();
+        var jdbc = new JDBC();
         var login = new LoginPage();
         login.login();
+        String customerName = jdbc.reader("select customer_fullname from customer, users where user_name='" + login.getUserName() +"'", "customer_fullname");
+        console.consolePrintLn(customerName);
+        String balance = jdbc.reader("select balance from customer, users where user_name='" + login.getUserName() + "'", "balance");
+        console.consolePrintLn("Balance: $" + balance);
     }
 }
