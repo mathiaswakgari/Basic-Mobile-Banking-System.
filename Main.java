cb808d5b0d747fcd5ae7e4d2460ece52b4a44150
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2022-10-12 18:05:31 +0300

    Fixed few bugs some neat system experience.
    Experience updates here and there. ironed out some bugs.

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 package com.mathias;
 
 import java.sql.SQLException;
+import java.util.Locale;
 import java.util.Objects;
-import java.util.Scanner;
+
 
 public class Main {
 
@@ -25,34 +26,53 @@ public class Main {
                 balanceConfigure.balanceReader(balanceConverted);
                 console.consolePrintLn("Balance: $" + new String(String.valueOf(balanceConfigure.getBalance())));
                 console.consolePrintLn("----------------------------");
+                String input = console.consoleInput("Would you like to select other options(Yes(y)/No(n))?: ");
+                input = input.toLowerCase(Locale.ROOT);
+                if (Objects.equals(input, "n")) {
+                    console.consolePrintLn();
+                    break;
+                }
             }
             else if (Objects.equals(choice, "2")){
-                String toWhom = console.consoleInput("Enter recipients full-name: ");
-                if (jdbc.userReader(toWhom)){
-                    String amount = console.consoleInput("Enter amount in numbers: ");
-                    double amountDouble = Double.parseDouble(amount);
-                    double balanceConverted = Double.parseDouble(balance);
-                    balanceConfigure.balanceReader(balanceConverted);
-                    if (amountDouble >= balanceConfigure.getBalance()){
-                        console.consolePrintLn("----------------------------");
-                        console.consolePrintLn("You don't have that much balance in your account. Try with legitimate amount.");
-                    }
-                    else{
-                        double deposit = balanceConfigure.getBalance() - amountDouble;
-                        jdbc.updater("update customer set balance = '" + deposit + "'where user_name='" + login.getUserName() + "'");
-                        jdbc.updater("update customer set balance=balance + '"+ amount +"'where customer_fullname='" + toWhom+"'");
-                        console.consolePrintLn("You have deposited $" + amountDouble + " to " + toWhom + ".");
-                        String updatedBalance = jdbc.reader("select balance from customer where user_name='" + login.getUserName() + "'", "balance");
-                        console.consolePrintLn("Balance: $" + updatedBalance);
-                        console.consolePrintLn("----------------------------");
+                String toWhom = console.consoleInput("Enter recipient's full-name: ");
+                if (Objects.equals(toWhom, customerName)) {
+                    console.consolePrintLn("Transaction Failed. Can't transfer to current account from current account.");
+                    String input = console.consoleInput("Would you like to select other options(Yes(y)/No(n))?: ");
+                    input = input.toLowerCase(Locale.ROOT);
+                    if (Objects.equals(input, "n")) {
+                        console.consolePrintLn();
+                        break;
                     }
                 }
                 else{
-                    console.consolePrintLn("The Name provided could not be directed to any one. Try a valid name.");
+                    if (jdbc.userReader(toWhom)){
+                        String amount = console.consoleInput("Enter amount in numbers: ");
+                        double amountDouble = Double.parseDouble(amount);
+                        double balanceConverted = Double.parseDouble(balance);
+                        balanceConfigure.balanceReader(balanceConverted);
+                        if (amountDouble >= balanceConfigure.getBalance()){
+                            console.consolePrintLn("----------------------------");
+                            console.consolePrintLn("You don't have that much balance in your account. Try with legitimate amount.");
+                        }
+                        else{
+                            double deposit = balanceConfigure.getBalance() - amountDouble;
+                            jdbc.updater("update customer set balance = '" + deposit + "'where user_name='" + login.getUserName() + "'");
+                            jdbc.updater("update customer set balance=balance + '"+ amount +"'where customer_fullname='" + toWhom+"'");
+                            console.consolePrintLn("You have deposited $" + amountDouble + " to " + toWhom + ".");
+                            String updatedBalance = jdbc.reader("select balance from customer where user_name='" + login.getUserName() + "'", "balance");
+                            console.consolePrintLn("Balance: $" + updatedBalance);
+                            console.consolePrintLn("----------------------------");
+                        }
+                    }
+                    else{
+                        console.consolePrintLn("The Name provided could not be directed to any one. Try a valid name.");
+                    }
+                    String input = console.consoleInput("Would you like to select other options(Yes(y)/No(n))?: ");
+                    input = input.toLowerCase(Locale.ROOT);
                 }
             }
             else if(Objects.equals(choice, "3")){
-                console.consolePrintLn("Thank-you for choosing this system.\nExiting..........");
+                console.consolePrintLn();
                 break;
             }
             else{
