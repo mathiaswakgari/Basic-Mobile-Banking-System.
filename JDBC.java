56c0b581b1fb70a5d12fb15d3e4044fc9341b822
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2022-10-13 17:21:26 +0300

    Transaction table reader.
    New transaction table reader from database.

--- JDBC.java
+++ JDBC.java
@@ -29,6 +29,18 @@ public class JDBC {
         return resultSet.next();
 
     }
+     public void transactionReader(String customerName) throws SQLException {
+        var console = new Console();
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mobile_banking","root","4779");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select transaction_date, deposit_amount, customer_depositedTo from p_transaction where customer_depositedFrom='" + customerName+"'");
+        while (resultSet.next()){
+            Date date = resultSet.getDate("transaction_date");
+            float depositAmount = resultSet.getFloat("deposit_amount");
+            String depositedTo = resultSet.getString("customer_depositedTo");
+            console.consolePrintLn("Deposited to: "+depositedTo+", Deposited amount: $"+depositAmount+", Date: "+date+" ;");
+            console.consolePrintLn("----------------------------");
+        }}
 
 }
 
